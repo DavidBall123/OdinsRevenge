c10e8910891eb8eb17cef97051babb19112ca5bf
David Ball <david_ball01@hotmail.com>
2012-05-07 17:48:09 +1000

    compression of database.

---
+++ OdinsRevenge/Game Management/ScoreManagement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+
+namespace OdinsRevenge
+{
+    class ScoreManagement
+    {
+
+        private const int MAX = 1000;
+
+        public string[] ReadScores()
+        {
+            string[] file = new string[MAX];
+            string line;
+            int count = 0;
+            StreamReader filereader = new StreamReader("scores.txt");
+            line = filereader.ReadLine();
+
+            while (line != null)
+            {
+                file[count] = line;
+                count++;
+                line = filereader.ReadLine();
+            }
+            filereader.Close();
+
+            return file;
+        }
+
+        private string[] SortScores(string[] scores)
+        {
+            string[] score = new string[MAX];
+            string temp;
+            int score1;
+            int score2;
+            string[] split1;
+
+            score = scores; 
+            return score;
+        }
+    }
+}
---
+++ OdinsRevenge/Screens/HighScoreScreen.cs
@@ -0,0 +1,132 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// BackgroundScreen.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace OdinsRevenge
+{
+    /// <summary>
+    /// The following class is modfied on a class found at this location
+    /// http://create.msdn.com/en-US/education/catalog/sample/game_state_management
+    /// 
+    /// The background screen sits behind all the other menu screens.
+    /// It draws a background image that remains fixed in place regardless
+    /// of whatever transitions the screens on top of it may be doing.
+    /// </summary>
+    class HighScoreScreen : GameScreen
+    {
+        #region Fields
+
+        ContentManager content;
+        Texture2D backgroundTexture;
+        private int closingCounter = 150;
+        private const int MAX = 1000;
+        
+        private string[] scores = new string[MAX];
+        ScoreManagement scoreManagement = new ScoreManagement();
+
+        #endregion
+
+        
+        #region Initialization
+        
+
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public HighScoreScreen()
+        {
+            TransitionOnTime = TimeSpan.FromSeconds(0.5);
+            TransitionOffTime = TimeSpan.FromSeconds(0.5);
+            scores = scoreManagement.ReadScores(); 
+        }
+
+
+        /// <summary>
+        /// Loads graphics content for this screen. The background texture is quite
+        /// big, so we use our own local ContentManager to load it. This allows us
+        /// to unload before going from the menus into the game itself, wheras if we
+        /// used the shared ContentManager provided by the Game class, the content
+        /// would remain loaded forever.
+        /// </summary>
+        public override void LoadContent()
+        {
+            if (content == null)
+                content = new ContentManager(ScreenManager.Game.Services, "Content");
+
+            backgroundTexture = content.Load<Texture2D>("Odin");
+        }
+
+
+        /// <summary>
+        /// Unloads graphics content for this screen.
+        /// </summary>
+        public override void UnloadContent()
+        {
+            content.Unload();
+        }
+
+
+        #endregion
+
+        #region Update and Draw
+
+
+        /// <summary>
+        /// Updates the background screen. Unlike most screens, this should not
+        /// transition off even if it has been covered by another screen: it is
+        /// supposed to be covered, after all! This overload forces the
+        /// coveredByOtherScreen parameter to false in order to stop the base
+        /// Update method wanting to transition off.
+        /// </summary>
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus,
+                                                       bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, false);
+            closingCounter--;
+            if (closingCounter <= 0)
+            {
+                ScreenManager.Game.Exit();
+                
+            }
+        }
+
+
+        /// <summary>
+        /// Draws the background screen.
+        /// </summary>
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch spriteBatch = ScreenManager.SpriteBatch;
+            Viewport viewport = ScreenManager.GraphicsDevice.Viewport;
+            Rectangle fullscreen = new Rectangle(0, 0, viewport.Width, viewport.Height);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(backgroundTexture, fullscreen,
+                             new Color(TransitionAlpha, TransitionAlpha, TransitionAlpha));
+            spriteBatch.DrawString(ScreenManager.Font, "Thank you for playing Odins Revenge", new Vector2(10, 100), Color.Red);
+            spriteBatch.DrawString(ScreenManager.Font, "By David Ball", new Vector2(250, 150), Color.Red); 
+
+            spriteBatch.End();
+        }
+
+       
+
+
+        #endregion
+    }
+}
+
